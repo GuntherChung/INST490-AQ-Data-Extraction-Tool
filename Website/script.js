5d676a9150db7ed8c35ad4e293bf8e165fd41466
Daniel H Gonzalez <raptor3500@hotmail.com>
2023-04-26 15:12:38 -0400

    showing error

--- Website/script.js
+++ Website/script.js
@@ -1,11 +1,27 @@
 const headers = { "X-API-Key": "C7AF49F7-C3F6-11ED-B6F4-42010A800007" }; // replace with your actual API key
 
-async function getSensorData(sensorId) {
-  const url = `https://api.purpleair.com/v1/sensors/${sensorId}`;
-  const response = await fetch(url, { headers });
-  const data = await response.json();
-  return data;
-}
+const sensors = {
+    Union_Bethel_AME: "133730#11.74", 
+    William_S_Schmidt: "134488#11.74",
+    PGCPS_Schmidt_CenterBldg: "102898#11",
+    Surratsville_HS: "102852#11.05",
+    Oxon_Hill_HS: "104790#11.05"
+    // add more sensors here as needed
+  };
+  
+
+  async function getSensorData(sensorIds) {
+    try {
+      const promises = sensorIds.map((sensorId) => {
+        const url = `https://api.purpleair.com/v1/sensors/${sensorId}`;
+        return fetch(url, { headers: headers }).then((response) => response.json());
+      });
+      const sensorData = await Promise.all(promises);
+      return sensorData;
+    } catch (error){
+        console.error(error);
+    }
+  }  
 
 async function getHistoricalData(sensorId, start="1weekago", end="now", interval="hourly") {
   const url = `https://api.purpleair.com/v1/sensors/${sensorId}/stats?from=${start}&to=${end}&interval=${interval}`;
@@ -14,23 +30,18 @@ async function getHistoricalData(sensorId, start="1weekago", end="now", interval
   return data;
 }
 
-async function getAllSensorIds() {
-  const url = "https://api.purpleair.com/v1/sensors";
-  const response = await fetch(url, { headers });
-  const data = await response.json();
-  const sensorIds = data.map(sensor => sensor["sensor_index"]);
-  return sensorIds;
-}
-
 async function getAllSensorData() {
-  const sensorIds = await getAllSensorIds();
-  const sensorData = {};
-  for (const sensorId of sensorIds) {
-    const data = await getSensorData(sensorId);
-    sensorData[sensorId] = data;
+    try {
+      const promises = Object.values(sensors).map((sensorId) => {
+        return getSensorData(sensorId);
+      });
+      const sensorData = await Promise.all(promises);
+      return sensorData;
+    } catch (error) {
+      console.error(error);
+    }
   }
-  return sensorData;
-}
+  
 
 async function getAllHistoricalData(start="1weekago", end="now", interval="hourly") {
   const sensorIds = await getAllSensorIds();
